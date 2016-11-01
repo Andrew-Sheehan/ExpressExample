69d99dd82e25aacd801303e177ac719bbf790b17
SliceDeCasa <zL0w3R6896>
2016-10-31 21:36:05 -0400

    updates with JSON

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ app.get("/test1", function(req, res) {
     res.send(req.query.name);
 });
 
+app.get("/json", function(req, res) {
+    res.setHeader("Content-Type", "application/json");
+    res.send(JSON.stringify({name: req.query.name, age: 33}));
+});
+
 app.listen(3000, function() {
     console.log("Listener started on port 3000");
 });
